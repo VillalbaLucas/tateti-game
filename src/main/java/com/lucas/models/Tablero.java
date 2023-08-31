300d05b8b24f3361efc1f4b41a2df7e45f5525b8
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-08-31 19:26:21 -0300

    create: cargar and show tablero
    agregue metodos donde cargo las posiciones en un map para hacer la conversion de que cuando se ingresa el 1 la posicvion es 0,0; la 2 es 0,1 etc... tambien metodo donde muestro lo que esta en el tablero

--- src/main/java/com/lucas/models/Tablero.java
+++ src/main/java/com/lucas/models/Tablero.java
@@ -1,19 +1,42 @@
 package com.lucas.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tablero {
-    private final char [][] tablero = new char[3][3];
+    private final char[][] tablero = new char[3][3];
+    private Map<Integer, int[]> posicion = new HashMap<>();
 
-    public Tablero(){}
-
-    public void marcarPosicion(char c, int[] pos){
-        tablero[pos[0]][pos[1]] = c;
+    public Tablero(){
+        cargarPosiciones();
+    }
+    private void cargarPosiciones(){
+        for (int i = 0; i < 9; i++){
+            int col = i%3, row = i/3;
+            posicion.put(i+1, new int[]{row, col});
+        }
     }
 
-    public char getPosicion(int[] pos){
+    public void marcarCasilla(char c, int[] pos){
+        tablero[pos[0]][pos[1]] = (char) c;
+    }
+
+    public  int[] getPosicion(int pos){
+        return posicion.get(pos);
+    }
+
+    public void showTablero(){
+        for(char[] i: tablero){
+            for (char j : i) {
+                if(j != ('X'|'O'))
+                    System.out.print('^' + " ");
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public char getSimbolIn(int[] pos){
         return tablero[pos[0]][pos[1]];
     }
 
-    public char[][] getTablero(){
-        return tablero;
-    }
 }
