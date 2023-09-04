fed41e9163fa19746b3283a69272f9d6d5f4b1d8
VillalbaLucas <lukasvillalba2016@gmail.com>
2023-09-03 22:40:43 -0300

    fix: show en Tablero

--- src/main/java/com/lucas/models/Tablero.java
+++ src/main/java/com/lucas/models/Tablero.java
@@ -2,14 +2,19 @@ package com.lucas.models;
 
 import java.util.HashMap;
 import java.util.Map;
+import static java.lang.System.out;
 
 public class Tablero {
-    private final char[][] tablero = new char[3][3];
+    private char[][] tablero = new char[3][3];
     private Map<Integer, int[]> posicion = new HashMap<>();
 
     public Tablero(){
         cargarPosiciones();
     }
+
+    /**
+     * Hace las conversiones de la posicion de la matriz a una posicion de solo numeros ej: la posicion 0,0 es el 1 y 2,1 es la 8
+     */
     private void cargarPosiciones(){
         for (int i = 0; i < 9; i++){
             int col = i%3, row = i/3;
@@ -27,16 +32,16 @@ public class Tablero {
 
     public void show(){
         for(char[] i: tablero){
-            for (char j : i) {
-                if(j != ('X'|'O'))
-                    System.out.print('^' + " ");
-                System.out.print(j + " ");
+            for (char j : i){
+                if(j != 'X' & j != 'O')
+                    out.print('^' + "  ");
+                else
+                    out.print(j + "  ");
             }
-            System.out.println();
+            out.println("\n");
         }
     }
     public char getSimbolIn(int[] pos){
         return tablero[pos[0]][pos[1]];
     }
-
 }
